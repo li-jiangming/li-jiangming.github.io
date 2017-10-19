ddbc3dc0ff20d6b5699f23aa52dfa9a3a655733a
Jiangming Li <lijiangming0910@hotmail.com>
2017-10-19 14:36:35 +0800

    add menu

--- static/js/footer_bottom.js
+++ static/js/footer_bottom.js
@@ -3,11 +3,15 @@ function footer_bottom(){
         var window_h = window.innerHeight;
         var window_w = window.innerWidth;
         var footer = document.getElementsByTagName("footer")[0];
+        var nav = document.getElementById("nav");
         if (window_w < 800) {
+                nav.setAttribute("class", "hidden");
                 footer.style.width = window_w;
         } else {
-                footer.style.width = 800;                                                                         
+                nav.removeAttribute("class");
+                footer.style.width = 800;
         }
+
         footer.removeAttribute("class");
         if (content_h < window_h - 50) {
                 footer.setAttribute("class", "fixed-bottom");
@@ -17,4 +21,15 @@ function footer_bottom(){
 footer_bottom();
 window.onresize = function() {
         footer_bottom();
+}
+
+function menu() {
+        var nav = document.getElementById("nav");
+        var cls = nav.getAttribute("class");
+
+        if (cls == "hidden") {
+                nav.removeAttribute("class");
+        } else {
+                nav.setAttribute("class", "hidden");
+        }
 }
